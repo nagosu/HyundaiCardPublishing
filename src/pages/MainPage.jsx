cd2e59d907ee65370bd98ad32229ce7dd684f1bb
nagosu <nozick1021@gmail.com>
2024-06-06 01:21:36 +0900

    퍼블리싱 저장포인트

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,23 @@
 import "../css/main.css";
+import musinsa from "../assets/videos/musinsa.mp4";
 
 function MainPage() {
   return (
     <div className='container'>
-      <div>HI</div>
+      {/* 백그라운드 비디오 */}
+      <video
+        className='video'
+        src={musinsa}
+        muted
+        loop
+        playsInline
+        autoPlay
+      ></video>
+      <div className='cardWrapper'>
+        <div className='cardSection'>
+          <div className='box'></div>
+        </div>
+      </div>
     </div>
   );
 }
